8a5bae9ae7da0295339dd3290373db529457dd6f
Ji Park <ji.park@jpnws.com>
2024-02-25 00:53:50 -0800

    Add one more test.

--- tests/api/accounts.test.js
+++ tests/api/accounts.test.js
@@ -26,19 +26,70 @@ describe("Accounts API", () => {
   });
 
   it("creates a new account and returns a success message", async () => {
+    // * ======================================================================
+    // * Arrange
+    // * ======================================================================
     const newAccount = {
       username: "tester",
       role: "admin",
     };
-
-    console.log(`http://localhost:${port}`);
-
+    // * ======================================================================
+    // * Act
+    // * ======================================================================
     const response = await request(`http://localhost:${port}`)
       .post("/accounts")
       .send(newAccount)
       .expect("Content-Type", /json/)
       .expect(201);
-
+    // * ======================================================================
+    // * Assert
+    // * ======================================================================
     expect(response.body).toEqual({ message: "Account created." });
+  });
+
+  it("inserts dummy accounts and returns a list of accounts", async () => {
+    // * ======================================================================
+    // * Arrange
+    // * ======================================================================
+    let accounts = [
+      {
+        username: "paulhal",
+        role: "admin",
+      },
+      {
+        username: "johndoe",
+        role: "member",
+      },
+      {
+        username: "sarahjane",
+        role: "member",
+      },
+    ];
+    for (const account of accounts) {
+      await db.query(
+        `
+        INSERT INTO
+          accounts (username, role)
+        VALUES
+          ($1, $2)
+        `,
+        [account.username, account.role]
+      );
+    }
+    // * ======================================================================
+    // * Act
+    // * ======================================================================
+    const response = await request(`http://localhost:${port}`)
+      .get("/accounts")
+      .expect("Content-Type", /json/)
+      .expect(200);
+    // * ======================================================================
+    // * Assert
+    // * ======================================================================
+    const responseAccounts = response.body;
+    for (let i = 0; i < accounts.length; i++) {
+      expect(responseAccounts[i].username).toEqual(accounts[i].username);
+      expect(responseAccounts[i].role).toEqual(accounts[i].role);
+    }
   });
 });
