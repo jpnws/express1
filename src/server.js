81a40598ac6aab42e2a4046e87724a771135566a
Ji Park <ji.park@jpnws.com>
2024-02-27 20:06:03 -0800

    Fix envs for production.

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const dbConfig = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL === "true",
+  ssl: process.env.DB_SSL === "true" || process.env.NODE_ENV === "production",
 };
 
 const pool = new pg.Pool(dbConfig);
